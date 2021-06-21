61713055e7a9c4d109999a995414e5e57fbee8ef
wertzuk <pascalb95@gmx.de>
2021-06-21 23:28:06 +0200

    added form

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// --------------------------
+// Bilder
+// --------------------------
+
 const images = document.querySelectorAll(".image");
 let len = images.length;
 let i = 0;
@@ -10,3 +14,55 @@ setInterval(() => {
   }
   images[i].classList.add("active");
 }, 7000);
+
+// --------------------------
+// Menü
+// --------------------------
+const navToggle = document.querySelector(".nav-toggle");
+const hamburger = document.querySelector(".hamburger");
+const closed = document.querySelector(".close");
+const menu = document.querySelector(".menu");
+
+navToggle.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  closed.classList.toggle("active");
+  menu.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
+
+// --------------------------
+// Kalender-Funktionalität
+// --------------------------
+
+const days = document.querySelectorAll(".days li");
+const monthHeader = document.querySelector(".month");
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
+
+const options = { month: "long" };
+const monthEnglish = new Intl.DateTimeFormat("en-US", options).format(today);
+const monthGerman = new Intl.DateTimeFormat("de-DE", options).format(today);
+
+const firstDayIndex = adjustDay(new Date(`${monthEnglish} 1, ${year}`));
+
+console.log(month);
+monthHeader.textContent = monthGerman + " " + year;
+
+for (let i = 0; i < 30; i++) {
+  days[firstDayIndex + i].textContent = i + 1;
+  days[firstDayIndex + i].addEventListener("click", () => {
+    days[firstDayIndex + i].classList.toggle("marked");
+  });
+}
+
+function adjustDay(date) {
+  const day = date.getDay();
+  if (day == 0) {
+    return 6;
+  }
+  return day - 1;
+}
+
+const todayIndex = adjustDay(today);
